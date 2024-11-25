006665fe42b9e293ea446ff86237f6cf8bab15fe
saisab29 <saisabk29@gmail.com>
2024-11-25 20:41:13 +0545

    Added pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
+"use client";
+import { useState } from "react";
 import { Card } from "./Component/Card";
 import data from "../data.json";
 
 export default function Home() {
+  const cardsPerPage = 8;
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  // Calculate the index range for the cards to display on the current page
+  const indexOfLastCard = currentPage * cardsPerPage;
+  const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+  const currentCards = data.slice(indexOfFirstCard, indexOfLastCard);
+
+  // Calculate the total number of pages
+  const totalPages = Math.ceil(data.length / cardsPerPage);
+
+  // Handle page change
+  const handlePageChange = (pageNumber: number): void => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
-    <div className=" sm:px-44 py-16">
-      <div className=" flex flex-wrap justify-around sm:justify-between">
-        {data.map((project) => (
+    <div className="sm:px-44 py-16 flex flex-col min-h-screen">
+      <div className="flex-grow flex flex-wrap justify-around sm:justify-between">
+        {currentCards.map((project) => (
           <Card
             key={project.id}
             id={project.id}
@@ -15,6 +33,42 @@ export default function Home() {
             description={project.description}
           />
         ))}
+      </div>
+
+      {/* Pagination Controls */}
+      <div className=" bottom-0 left-0 right-0 py-4  flex justify-center">
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+          className={`px-4 py-2 mx-2 border rounded-full bg-[#D9D9D9] ${
+            currentPage === 1 ? "text-[#6D7D8B]" : ""
+          } `}
+        >
+          Previous
+        </button>
+
+        {/*Page Numbers*/}
+        {[...Array(totalPages)].map((_, index) => (
+          <button
+            key={index}
+            onClick={() => handlePageChange(index + 1)}
+            className={`px-4 py-2 mx-2 border rounded-full ${
+              currentPage === index + 1
+                ? "bg-black text-white"
+                : "bg-[#D9D9D9] text-black"
+            }`}
+          >
+            {index + 1}
+          </button>
+        ))}
+
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+          className="px-4 py-2 mx-2 border rounded-full bg-[#D9D9D9]"
+        >
+          Next
+        </button>
       </div>
     </div>
   );
--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -8,7 +8,7 @@ export default async function Project({ params }: { params: { id: string } }) {
   const project = data.find((project) => project.id === id);
   return (
     <>
-      <div className=" flex-col w-9/12 m-auto overflow-x-hidden">
+      <div className="flex-col w-9/12 m-auto overflow-x-hidden py-4">
         <div className="py-4">
           <div className="flex">
             <Image
@@ -33,39 +33,40 @@ export default async function Project({ params }: { params: { id: string } }) {
           <button className="sm:hidden justify-center rounded-lg text-white bg-[#1A80E5] hover:bg-white hover:text-[#1A80E5] w-full py-1 my-8 border-2 border-[#1A80E5]">
             Visit
           </button>
-          <div className="flex  gap-4">
-            {/* Not using Image from next for now because hostname is not configured under images in your `next.config.js`*/}
-            <div className="hidden sm:block flex-col gap-4 my-8 border border-neutral-500 w-2/6">
+          <div className="flex gap-4 w-full">
+            {/* QuickStart Section */}
+            <div className="hidden xl:block flex-col my-8 border border-neutral-500 w-3/12 p-4">
               <h1 className="font-bold">
                 QuickStart Plutus and Cardano Development
               </h1>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
-                blanditiis id commodi laudantium quos autem labore animi. Neque,
-                illo facere.
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. At,
+                vel?
               </p>
               <button className="bg-red-500 text-white p-2">
                 Try Kuber IDE
               </button>
             </div>
 
-            <div className="image flex gap-4 overflow-x-auto my-8 h-2/6">
+            {/* Image Section */}
+            <div className="flex gap-4 overflow-x-auto my-8 h-2/6 sm:w-9/12">
               <img
-                className="sm:w-3/12 rounded h-2/6"
+                className="rounded h-2/6 sm:w-auto"
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMdgJXE2MbWrMAX_9HbjS3PaFiYK-FkqIctA&s"
                 alt="Screenshot"
                 width={200}
                 height={200}
-              ></img>
+              />
               <img
-                className="sm:w-3/12 rounded h-2/6"
+                className="rounded h-2/6 sm:w-auto"
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMdgJXE2MbWrMAX_9HbjS3PaFiYK-FkqIctA&s"
                 alt="Screenshot"
                 width={200}
                 height={200}
-              ></img>
+              />
             </div>
           </div>
+
           <div>
             <h2 className="font-bold">About this app</h2>
             <p>{project?.about}</p>
