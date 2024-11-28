7b8b3a0e6b449b9e9a5d4badf5880242677e1650
saisab29 <saisabk29@gmail.com>
2024-11-28 13:43:46 +0545

    Link to similar projects

--- src/app/Component/SimilarProjects.tsx
+++ src/app/Component/SimilarProjects.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import data from "../../data.json";
+import Link from "next/link";
 
 interface SimilarProjectsProps {
   currentProjectId: string; // or number, depending on the type of `id`
@@ -15,9 +16,10 @@ export default function SimilarProjects({
           .filter((project) => project.id !== currentProjectId)
           .slice(0, 4)
           .map((project) => (
-            <div
+            <Link
+              href={`/projects/${project.id}`}
               key={project.id}
-              className="flex py-3 w-60 items-center gap-2 h-24"
+              className="flex py-3 w-60 items-center gap-2 h-24 hover:text-[#1A80E5] group"
             >
               <div className="w-3/6 h-full">
                 <Image
@@ -29,10 +31,12 @@ export default function SimilarProjects({
                 />
               </div>
               <div className="w-4/6 max-h-full">
-                <h2>{project.projectName}</h2>
-                <p>Vice Studio</p>
+                <h2 className="font-normal text-lg ">{project.projectName}</h2>
+                <p className="font-normal text-sm text-[#9D9D9D] group-hover:text-[#1A80E5]">
+                  Vice Studio
+                </p>
               </div>
-            </div>
+            </Link>
           ))
       }
     </>
